65bd506730906a409954487a69f7d3aaadc43767
victor <victorvj26@gmail.com>
2012-10-23 21:25:37 +0200

    Constants

---
+++ Classes/Constants.h
@@ -0,0 +1,25 @@
+//
+//  Constants.h
+//  Draw2
+//
+//  Created by Victor Valle Juarranz on 23/10/12.
+//
+//
+
+
+#pragma mark -
+#pragma mark Distances
+
+/**
+ * Defines the distance between two points in a gesture
+ */
+#define CONSTANT_DISTANCE                                               10.0f
+
+
+#pragma mark -
+#pragma mark Colors definition
+
+/**
+ * Defines the color red
+ */
+#define COLOR_RED                                                       [UIColor redColor]
